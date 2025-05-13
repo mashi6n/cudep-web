cec5e18bb9965fc6b1153a0609b5361f9110f8b3
Mashiro Toyooka <92073972+mashi6n@users.noreply.github.com>
2025-05-14 02:34:18 +0900

    feat: ページの形式が変わったのでロジックを変更

--- scripts/scrape-gpu.ts
+++ scripts/scrape-gpu.ts
@@ -9,24 +9,72 @@ async function ScrapeGPU(): Promise<Gpu[]> {
   const url = "https://developer.nvidia.com/cuda-gpus"
   const dom = await JSDOM.fromURL(url)
   const doc = dom.window.document
-  const trs = doc.querySelectorAll("tr")
+
+  const { Node } = dom.window
+  const TEXT_NODE = Node.TEXT_NODE
+  const ELEMENT_NODE = Node.ELEMENT_NODE
+
+  function isElement(node: Node): node is Element {
+    return node.nodeType === ELEMENT_NODE
+  }
+
+  // get text content of an element and its children, handling <br> tags as '\n'
+  function getText(element: Element): string {
+    let text = ""
+    if (element.nodeType === TEXT_NODE) {
+      text = element.textContent ?? ""
+    }
+    for (const child of element.childNodes) {
+      if (child.nodeType === TEXT_NODE) {
+        text += child.textContent ?? ""
+      } else if (isElement(child)) {
+        if (child.tagName === "BR") {
+          text += "\n"
+        } else {
+          text += getText(child)
+        }
+      }
+    }
+    return text
+  }
+
+  const tableBody = doc.querySelector("#isy0j > tbody")
+  if (tableBody === null) {
+    console.error("Table body not found")
+    return gpus
+  }
+
+  const trs = tableBody.querySelectorAll("tr")
   trs.forEach((tr) => {
-    const gpu = parseRow(tr)
-    if (gpu && !seenGpus.has(gpu.name)) {
-      seenGpus.add(gpu.name)
-      gpus.push(gpu)
+    for (const gpu of parseRow(tr, getText)) {
+      if (!seenGpus.has(gpu.name)) {
+        seenGpus.add(gpu.name)
+        gpus.push(gpu)
+      }
     }
   })
+
   return gpus
 }
 
-function parseRow(row: Element): Gpu | null {
+function parseRow(row: Element, getText: (elm: Element) => string): Gpu[] {
   const cells = row.querySelectorAll("td")
   if (cells.length < 2) {
-    return null
+    console.error("Row does not have enough cells")
+    return []
   }
-  const gpuName = cells[0].textContent?.trim() ?? ""
-  const cc: string = cells[1].textContent?.trim() ?? ""
-  return new Gpu(gpuName.trim(), CompCap.fromString(cc))
+
+  const cc = cells[0].textContent?.trim() ?? ""
+
+  const gpuNamesText = getText(cells[1]) + " " + getText(cells[2]) + getText(cells[3])
+  if (gpuNamesText === "") {
+    console.error("GPU name not found")
+    return []
+  }
+
+  return gpuNamesText.split("\n").map((name) => {
+    return new Gpu(name.trim(), CompCap.fromString(cc))
+  })
 }
+
 export default ScrapeGPU
